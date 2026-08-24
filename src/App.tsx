8d33663786e5f552d829a77504f34dd1c0c40607
yashaschandru583-glitch <yashaschandru583@gmail.com>
2026-08-24 19:29:25 +0530

    Switch from BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+
 import { ToastProvider } from './context/ToastContext';
 import { AuthProvider } from './context/AuthContext';
+
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 
@@ -21,22 +28,77 @@ export default function App() {
       <ToastProvider>
         <AuthProvider>
           <div className="min-h-screen flex flex-col bg-[#050505] text-[#E5E5E5] selection:bg-[#FF2B2B] selection:text-white">
+
             <Navbar />
+
             <main className="flex-1">
               <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/explore" element={<ExplorePage />} />
-                <Route path="/post/:id" element={<PostDetailPage />} />
-                <Route path="/create" element={<CreateEditPostPage />} />
-                <Route path="/edit/:id" element={<CreateEditPostPage />} />
-                <Route path="/my-posts" element={<MyPostsPage />} />
-                <Route path="/profile" element={<ProfilePage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="*" element={<Navigate to="/" replace />} />
+
+                {/* Home */}
+                <Route
+                  path="/"
+                  element={<HomePage />}
+                />
+
+                {/* Explore */}
+                <Route
+                  path="/explore"
+                  element={<ExplorePage />}
+                />
+
+                {/* Post */}
+                <Route
+                  path="/post/:id"
+                  element={<PostDetailPage />}
+                />
+
+                {/* Create */}
+                <Route
+                  path="/create"
+                  element={<CreateEditPostPage />}
+                />
+
+                {/* Edit */}
+                <Route
+                  path="/edit/:id"
+                  element={<CreateEditPostPage />}
+                />
+
+                {/* My Posts */}
+                <Route
+                  path="/my-posts"
+                  element={<MyPostsPage />}
+                />
+
+                {/* Profile */}
+                <Route
+                  path="/profile"
+                  element={<ProfilePage />}
+                />
+
+                {/* Login */}
+                <Route
+                  path="/login"
+                  element={<LoginPage />}
+                />
+
+                {/* Register */}
+                <Route
+                  path="/register"
+                  element={<RegisterPage />}
+                />
+
+                {/* Unknown route */}
+                <Route
+                  path="*"
+                  element={<Navigate to="/" replace />}
+                />
+
               </Routes>
             </main>
+
             <Footer />
+
           </div>
         </AuthProvider>
       </ToastProvider>
